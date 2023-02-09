a12126200ed3f45701566d22baea0d2c434e452c
aleee-git <alejandraramirez.niu@nehemiahiu.com>
2023-02-08 23:03:08 -0600

    Estilo Listado Pacientes

--- src/Components/ListadoPacientes.jsx
+++ src/Components/ListadoPacientes.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Paciente from './Paciente';
 
 export default function ListadoPacientes() {
   return (
-    <div className='md:w-1/2 lg:w-3/5'>
+    <div className='md:w-1/2 lg:w-3/5 p-10 md:h-screen overflow-y-scroll'>
 
-      <h2> Listado de Pacientes </h2>
+      <h2 className='font-black text-center text-3xl'> Listado Pacientes </h2>
+      <p className='text-xl mt-5 text-center mb-10'> Administra tus {""} <span className='text-lime-600 font-bold'>Pacientes y citas</span></p>
+
+      <Paciente/>
+      <Paciente/>
+      <Paciente/>
+      <Paciente/>
+      <Paciente/>
+      <Paciente/>
+
     </div>
   );
 }
---
+++ src/Components/Paciente.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Paciente() {
+  return (
+    <div className='m-3 bg-white shadow-md rounded-lg p-10 mb-10'>
+
+    <p className='font-bold mb-3 text-gray-800 uppercase'> Nombre: {""} <span className='font-normal normal-case'>Hook</span></p>
+
+    <p className='font-bold mb-3 text-gray-800 uppercase'> Propietario: {""} <span className='font-normal normal-case'>Hook</span></p>
+
+    <p className='font-bold mb-3 text-gray-800 uppercase'> Email: {""} <span className='font-normal normal-case'>Hook</span></p>
+
+    <p className='font-bold mb-3 text-gray-800 uppercase'> Fecha Alta: {""} <span className='font-normal normal-case'>Hook</span></p>
+
+    <p className='font-bold mb-3 text-gray-800 uppercase'> Sintomas: {""} <span className='font-normal normal-case'>Lorem ipsum 
+    dolor sit, amet consectetur adipisicing elit. Voluptates consectetur labore minus ad quia non fuga voluptatibus! 
+    Nesciunt quae nihil deleniti dolore quo nulla impedit facere numquam iusto, molestias optio.</span></p>
+    
+  </div>
+  
+  );
+}
