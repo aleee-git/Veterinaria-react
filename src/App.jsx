3ffc8a0f24060452dc26ba414ae9f2da666dc1f9
aleee-git <alejandraramirez.niu@nehemiahiu.com>
2023-02-08 20:57:31 -0600

    Creacion de App, Instalacion de Dependencias y General Config

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './Components/Header'
+import ListadoPacientes from './Components/ListadoPacientes'
+
+function App() {
+
+  return (
+    <div className='container mx-auto mt-20'>
+      <Header/>
+      <ListadoPacientes/>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Archivos que usaran tailwind
+  content: ["./index.html", "./src**/*.jsx", "./src/Components/Header.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
