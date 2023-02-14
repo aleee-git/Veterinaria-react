36950a1c8d032d62e481b4216fce4b8ef2bdb134
aleee-git <alejandraramirez.niu@nehemiahiu.com>
2023-02-14 11:44:39 -0600

    Detalles

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,8 @@
 function Header () {
 
     return (
-        <h1 className="font-black text-5xl text-center w-1/2 mx-auto md:w-2/3"> Seguimiento Pacientes {''}
-        
-        <span className="text-lime-600">Veterinaria </span></h1>
+        <h1 className="text-center font-black text-3xl w-1/2 mx-auto md:w-2/3"> Seguimiento Pacientes {''} 
+        <span className="text-lime-600">Veterinaria</span></h1>
     )
 }
 
